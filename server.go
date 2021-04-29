c48befa5aeb41ef78edc6436e0eb8c6fd83165a1
jimc <jimc@jimcs-MacBook-Pro.local>
2021-04-29 18:49:07 +0800

    fixed unsynchronized pipes

--- server.go
+++ server.go
@@ -3,18 +3,32 @@ package main
 import (
 	"fmt"
 	"os"
+	"sync"
 	"syscall"
 )
 
 var writePipes []string = []string{"/tmp/alltoP1", "/tmp/alltoP2"}
 var readPipes []string = []string{"/tmp/allfromP1", "/tmp/allfromP2"}
-var saying []string = []string{"Hello one!", "Hello two!"}
+var saying []string = []string{"Hello one!\n", "Hello two!\n"}
 var clientNumbers int = 2
 
 func errorCheck(err error) {
 	if err != nil {
 		fmt.Println(err)
 	}
+}
+
+func writeToPipe(wg *sync.WaitGroup, path string, msg string) {
+	err := os.WriteFile(path, []byte(msg), 0)
+	errorCheck(err)
+	wg.Done()
+}
+
+func readFromPipe(path string) string {
+	//fmt.Println(path)
+	data, err := os.ReadFile(path)
+	errorCheck(err)
+	return string(data)
 }
 
 func main() {
@@ -25,18 +39,18 @@ func main() {
 	}
 
 	// Control logic
+	var wg sync.WaitGroup
 	var counter int = 0
 	for counter != clientNumbers {
-		// Write to pipes
-		for i := 0; i < clientNumbers; i++ {
-			err := os.WriteFile(writePipes[i], []byte(saying[counter%clientNumbers]), 0)
-			errorCheck(err)
+		for i := counter; i < clientNumbers; i++ {
+			wg.Add(1)
+			go writeToPipe(&wg, writePipes[i], saying[counter%clientNumbers])
 		}
 
 		// Read from pipe
-		data, err := os.ReadFile(readPipes[counter%clientNumbers])
-		errorCheck(err)
-		fmt.Println(string(data))
+		msg := readFromPipe(readPipes[counter%clientNumbers])
+		fmt.Println(msg)
+		wg.Wait()
 		counter++
 	}
 
